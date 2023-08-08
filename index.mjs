b9ce6a6863e89a13418c633139e561af596c1ac9
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-08-08 12:42:07 +0100

    `debug`

--- index.mjs
+++ index.mjs
@@ -1,4 +1,8 @@
+import debug from 'debug'
+
 import express from 'express'
+
+const log = debug('web-components')
 
 const app = express()
 const port = 3000
@@ -6,7 +10,7 @@ const port = 3000
 app.use('/assets', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.sendFile('index.html', { root: '.' })
 })
 
-app.listen(port, () => { console.log(port) })
+app.listen(port, () => { log(port) })
--- public/js/components/sequence-media.mjs
+++ public/js/components/sequence-media.mjs
@@ -20,12 +20,21 @@ class SequenceMedia extends HTMLElement {
 
     const sequenceMedia = document.createElement('div')
     sequenceMedia.setAttribute('class', 'sequence-media')
+    sequenceMedia.addEventListener('click', (e) => {
+      console.log('sequenceMedia click', e)
+    })
 
     const text = this.getAttribute('data-text')
     sequenceMedia.textContent = text
 
     shadowRoot.appendChild(document.getElementById('sequence-media').cloneNode(true))
     shadowRoot.appendChild(sequenceMedia)
+  }
+
+  connectedCallback () {
+    this.shadowRoot.addEventListener('click', (e) => {
+      console.log('shadowRoot click', e)
+    })
   }
 }
 
